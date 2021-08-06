5d437bdedb74e131ad78504ae1cca0feee4f6480
Arya Irama Wahono <aryairama987@gmail.com>
2021-08-06 19:18:29 +0700

    initial express

---
+++ src/helpers/helpers.js
@@ -0,0 +1,42 @@
+const response = (res, status, statusCode, message, data) => {
+  res.status(statusCode).json({
+    status,
+    statusCode,
+    message,
+    data,
+  });
+};
+
+const responsePagination = (res, status, statusCode, message, data, pagination) => {
+  res.status(statusCode).json({
+    status,
+    statusCode,
+    message,
+    data,
+    pagination,
+  });
+};
+
+const responseError = (res, status, statusCode, message, error) => {
+  res.status(statusCode).json({
+    status,
+    statusCode,
+    message,
+    error,
+  });
+};
+
+const promiseResolveReject = (resolve, reject, error, result) => {
+  if (!error) {
+    resolve(result);
+  } else {
+    reject(error);
+  }
+};
+
+export default {
+  response,
+  responseError,
+  promiseResolveReject,
+  responsePagination,
+};
