2200e1375ea532552d3c6fcdcc42b45196e94377
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-08-11 20:41:19 +0700

    add get top up

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -12,6 +12,7 @@ router
   .get('/gettransactions', Auth, Role('member', 'admin'), ControllerMain.getAllTransaction)
   .get('/showtransaction/:transactionId', Auth, Role('member', 'admin'), ControllerMain.showtransaction)
   .post('/topup', Auth, Role('member'), rulesFileUploud, rulesCreateImgTopUp(), topUpFieldRules(), resultOfValidation, ControllerMain.topUp)
+  .get('/gettopup', Auth, Role('admin'), ControllerMain.getTopup)
   .post('/updatetransaction', Auth, Role('admin'), statusRules(), resultOfValidation, ControllerMain.updatetransaction)
   .post('/transfer', Auth, Role('member'), transferFielfRules(), resultOfValidation, ControllerMain.transfer)
   .post('/cekpin', Auth, Role('member'), checkpin(), resultOfValidation, ControllerMain.checkPIN);
